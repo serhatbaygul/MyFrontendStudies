b341b8c6e79867810dac041a404237ee83591030
Serhat Baygül <107368369+serhatbaygul@users.noreply.github.com>
2023-06-10 23:53:00 +0300

    Add files via upload

---
+++ Nodejs/Global and Local Package/package1/index.js
@@ -0,0 +1,5 @@
+console.log("Package1");
+
+const is = require("simple-is");
+
+console.log(is.number(24));
---
+++ Nodejs/Global and Local Package/package2/index.js
@@ -0,0 +1,5 @@
+console.log("Package2");
+
+const is = require("simple-is");
+
+console.log(is.number(24));
